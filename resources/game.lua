c1041db048086f3ca39cc9b9cf5aef4bcc9d8e7b
luke-ohara <luke.c.ohara@gmail.com>
2014-12-10 12:18:09 +0000

    adding flappy object and physics

--- resources/game.lua
+++ resources/game.lua
@@ -1,3 +1,14 @@
 gameScene = director:createScene()
+ 
+local background = director:createSprite(director.displayCenterX, director.displayCenterY, "gfx/background.png")
 
-local label = director:createLabel(0,0, "Hey - Welcome to the game scene. \nCome here often?")
+background.xAnchor = 0.5
+background.yAnchor = 0.5
+background.rotation = 180
+
+local player = director:createSprite(director.displayCenterX/2, director.displayCenterY + (director.displayCenterY/2), "gfx/player.png")
+
+player.xAnchor = 0.5
+player.xAnchor = 0.5
+
+physics:addNode(player, {radius = 21})
--- resources/menu.lua
+++ resources/menu.lua
@@ -10,6 +10,14 @@ background.yAnchor = 0.5
 local gameTitle = director:createLabel(0, director.displayCenterY+(director.displayCenterY/2), "Flappy Beard\nBy Luke O'Hara")
 gameTitle.color = color.red
 
+-- Starts game when playbutton pressed
+
+function newGame(event)
+  if event.phase == "ended" then
+    switchToScene("game")
+  end
+end
+
 -- Create playbutton for users
 
 local playButton = director:createSprite(director.displayCenterX, director.displayCenterY, "gfx/start.png")
@@ -18,10 +26,3 @@ playButton.xAnchor = 0.5
 playButton.yAnchor = 0.5
 playButton:addEventListener("touch", newGame)
 
--- Starts game when playbutton pressed
-
-function newGame(event)
-  if event.phase == "ended" then
-    switchToScene("game")
-  end
-end
