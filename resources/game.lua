93858168d508d676ebe21cf3755c5515349a5a2f
luke-ohara <luke.c.ohara@gmail.com>
2014-12-10 12:00:08 +0000

    adding title and eventlistener to start button

---
+++ resources/game.lua
@@ -0,0 +1,3 @@
+gameScene = director:createScene()
+
+local label = director:createLabel(0,0, "Hey - Welcome to the game scene. \nCome here often?")
--- resources/menu.lua
+++ resources/menu.lua
@@ -6,9 +6,22 @@ local background = director:createSprite(director.displayCenterX, director.displ
 background.xAnchor = 0.5 
 background.yAnchor = 0.5 
 
+-- Game title 
+local gameTitle = director:createLabel(0, director.displayCenterY+(director.displayCenterY/2), "Flappy Beard\nBy Luke O'Hara")
+gameTitle.color = color.red
+
 -- Create playbutton for users
 
 local playButton = director:createSprite(director.displayCenterX, director.displayCenterY, "gfx/start.png")
 
 playButton.xAnchor = 0.5
 playButton.yAnchor = 0.5
+playButton:addEventListener("touch", newGame)
+
+-- Starts game when playbutton pressed
+
+function newGame(event)
+  if event.phase == "ended" then
+    switchToScene("game")
+  end
+end
