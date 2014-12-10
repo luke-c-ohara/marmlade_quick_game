3bcbd2cea7bfd8982c4c4226795ada8f4cb8e54c
luke-ohara <luke.c.ohara@gmail.com>
2014-12-10 16:38:00 +0000

    applying collisions to the columns and stopping the game when event happens

--- resources/game.lua
+++ resources/game.lua
@@ -2,11 +2,13 @@
 require("class")
 require("object")
 
+local gamePlaying = false
 local gameTaps = 0
 physics:setGravity(0, -980*1.5)
 local maxEnemies = 6
 local heightAdder = 750
 local widthAdder = 600
+
 
 gameScene = director:createScene()
 
@@ -19,9 +21,9 @@ background.rotation = 180
 
 -- adds users image
 local player = director:createSprite(director.displayCenterX/2, director.displayCenterY + (director.displayCenterY/2), "gfx/player.png")
-
 player.xAnchor = 0.5
 player.xAnchor = 0.5
+player.name ="player"
 
 --creating 6 enemies
 local yx = math.random(director.displayCenterY - 350, director.displayCenterY - 100)
@@ -34,10 +36,12 @@ for i=1,maxEnemies
   enemyArray[i] = object.new(i, "gfx/enemy.png")
   enemyArray[i]:getSprite().xAnchor = 0.5
   enemyArray[i]:getSprite().yAnchor = 0.5
+  enemyArray[i]:getSprite().name = "enemy"
   enemyArray[i]:getSprite().x = director.displayCenterX
   physics:addNode(enemyArray[i]:getSprite(), {type="kinematic"})
 end
 
+-- sets enemys up as pairs on top and bottom of the screen.
 local a = enemyArray[1]:getSprite().x
 local b = enemyArray[2]:getSprite().x
 local c = enemyArray[3]:getSprite().x - ((director.displayCenterX / 2)*2)
@@ -45,6 +49,7 @@ local d = enemyArray[4]:getSprite().x - ((director.displayCenterX / 2)*2)
 local e = enemyArray[5]:getSprite().x + ((director.displayCenterX / 2)*2)
 local f = enemyArray[6]:getSprite().x + ((director.displayCenterX / 2)*2)
 
+-- gets columns from the left to reappear on the right.
 enemyArray[1]:getSprite().physics:setTransform(a + widthAdder, yx, 0)
 enemyArray[2]:getSprite().physics:setTransform(b + widthAdder, yx+heightAdder, 0)
 enemyArray[3]:getSprite().physics:setTransform(c + widthAdder, yy, 0)
@@ -56,10 +61,25 @@ enemyArray[6]:getSprite().physics:setTransform(f + widthAdder, yz+heightAdder, 0
 local startGameHelp = director:createLabel(0, director.displayCenterY, "Tap to start!")
 startGameHelp.color = color.red
 
+-- looking for collison events
+function hit(event)
+  if event.phase == "began" then
+    if event.nodeA.name == "player" then
+      if event.nodeB.name == "enemy" then
+        gameover()
+      end
+    elseif event.nodeB.name == "player" then
+      if event.nodeA.name == "enemy" then
+        gameover()
+      end
+    end
+  end
+end
+
 -- Event listener: touch
 function systemEvents(event)
   if event.phase == "began" then
-    if player.physics then
+    if player.physics and gamePlaying == true then
       jump()
     end
   elseif event.phase == "ended" then
@@ -75,18 +95,26 @@ system:addEventListener("touch", systemEvents)
 
 -- game code, defines what the game does
 function init()
-  physics:addNode(player, {radius = 21})
   -- remove help label
   startGameHelp.alpha = 0
+  physics:addNode(player, {radius = 21}) 
   for i=1,maxEnemies
     do
       enemyArray[i]:getSprite().physics:setLinearVelocity(-160, 0)
+      enemyArray[i]:getSprite():addEventListener("collision", hit)
   end
+  gamePlaying = true
 end
 
 function gameover()
   local startGameHelp = director:createLabel(0, director.displayCenterY, "YOU LOST BRO! YOU LOST!")
   background.color = color.red
+  gamePlaying = false
+
+  for i=1,maxEnemies
+    do
+    enemyArray[i]:getSprite().physics:setLinearVelocity(0, 0)
+  end
 end
 
 function reset()
@@ -104,7 +132,7 @@ function updater()
   if player.y < 0 then
     gameover()
   end
-
+  -- constantly updated the columns 
   if (enemyArray[1]:getSprite().x + enemyArray[1]:getSprite().w) + 30 < 0 then
       local y = math.random(director.displayCenterY - 350, director.displayCenterY - 100)
       enemyArray[1]:getSprite().physics:setTransform(director.displayWidth + enemyArray[1]:getSprite().w, y, 0)
